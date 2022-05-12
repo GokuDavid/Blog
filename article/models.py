d12c32429fedf1a9014410131e6a8ccca80eccbb
goku <goku@goku.com>
2022-05-12 23:33:08 +0800

    add articlelist api

--- article/models.py
+++ article/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.utils import timezone
+
+
+class Article(models.Model):
+
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    created=models.DateTimeField(default=timezone.now)
+    updated=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
--- article/views.py
+++ article/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+from article.models import Article
+from rest_framework.views import APIView
+from .serializers import ArticleSerializer
+from django.http import Http404
+
+class ArticleList(APIView):
+    def get(self,request,format=None):
+        articles=Articles.objects.all()
+        serializer=ArticleSerializer(articles,many=True)
+        return Response(serializer.data)
+
+    def post(self,request,format=None):
+        serializer=ArticleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(author=request.user)
+            return Response(request.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
