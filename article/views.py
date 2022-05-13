dc75f1e4b5937f0a38f55ae7ec8de75dbabe82af
David <fasds@163.com>
2022-05-13 11:40:08 +0800

    add articledetail

--- article/views.py
+++ article/views.py
@@ -1,21 +1,47 @@
 from django.shortcuts import render
-
-# Create your views here.
 from article.models import Article
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import ArticleSerializer
 from django.http import Http404
 
+
 class ArticleList(APIView):
-    def get(self,request,format=None):
-        articles=Article.objects.all()
-        serializer=ArticleSerializer(articles,many=True)
+    def get(self, request, format=None):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
         return Response(serializer.data)
 
-    def post(self,request,format=None):
-        serializer=ArticleSerializer(data=request.data)
+    def post(self, request, format=None):
+        serializer = ArticleSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save(author=request.user)
-            return Response(request.data,status=status.HTTP_201_CREATED)
+            serializer.save()
+            return Response(request.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ArticleDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Article.objects.get(pk=pk)
+        except Article.DoesNotExist:
+            raise Http404
+
+    def get(self, request,pk, format=None):
+        article = self.get_object(pk)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+    
+    def put(self, request, pk, format=None):
+        article = self.get_object(pk)
+        serializer = ArticleSerializer(article,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request,pk, format=None):
+        article = self.get_object(pk)
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
