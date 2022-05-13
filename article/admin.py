abfbd5093324ef0d62232d04cfc9285b366b53c5
David <fasds@163.com>
2022-05-13 18:26:01 +0800

    add element-plus

--- article/admin.py
+++ article/admin.py
@@ -1,13 +1,9 @@
 from django.contrib import admin
 from .models import Article
 
-
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ['title', 'author', 'created']
-
     list_filter = ['author', ]
-
     list_per_page = 10
-
 
 admin.site.register(Article, ArticleAdmin)
