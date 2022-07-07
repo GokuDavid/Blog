d42c2e22673c4cdceeea8253c0518b3de0631837
goku <fasds@163.com>
2022-07-07 11:25:19 +0800

    增加axios，前后端交换数据

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <a-layout-content style="padding: 0 50px">
+    <a-breadcrumb style="margin: 16px 0">
+      <a-breadcrumb-item>Home</a-breadcrumb-item>
+      <a-breadcrumb-item>List</a-breadcrumb-item>
+      <a-breadcrumb-item>App</a-breadcrumb-item>
+    </a-breadcrumb>
+    <div :style="{ background: '#fff', padding: '24px', minHeight: '280px' }">
+      Content
+    </div>
+  </a-layout-content>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { defineComponent } from "vue";
+import axios from "axios";
+// import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 export default defineComponent({
-  name: 'Home',
-  components: {
-    HelloWorld,
-  },
+  name: "Home",
+  setup(){
+    console.log("setup")
+    axios.get("/api/article/").then(
+        (response)=>{
+          console.log(response)
+        }
+    )
+  }
 });
 </script>
